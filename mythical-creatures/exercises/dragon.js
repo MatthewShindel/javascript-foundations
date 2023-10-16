6636152886be7c063872c1f9ef8a601ed640a897
MatthewShindel <matthew@shindel.com>
2023-10-16 18:37:58 -0400

    complete dragon methods

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,38 @@
+function createDragon(name, rider, temperment){
+  return dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true,
+    fireBreather: (temperment === "aggressive")
+  };
+}
+
+function greetRider(dragon){
+  return `Hi, ${dragon.rider}!`
+}
+function eat(dragon){
+  dragon.timesEaten = dragon.timesEaten + 1;
+  if (dragon.timesEaten >= 3){
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+function findFireBreathers(arrayOfDragons){
+  var finalArray = [];
+  for(var i = 0; i < arrayOfDragons.length; i++){
+    if(arrayOfDragons[i].fireBreather){
+      finalArray.push(arrayOfDragons[i])
+    }
+  }
+  return finalArray;
+}
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
